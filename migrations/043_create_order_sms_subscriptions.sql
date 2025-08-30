9e58e5f9e24061a7b4424198c7c29803b83cbe8a
bradhawkins85 <bradhawkins@gmail.com>
2025-08-31 00:18:06 +1000

    Cascade user deletions

--- migrations/043_create_order_sms_subscriptions.sql
+++ migrations/043_create_order_sms_subscriptions.sql
@@ -2,5 +2,5 @@ CREATE TABLE order_sms_subscriptions (
   order_number VARCHAR(20) NOT NULL,
   user_id INT NOT NULL,
   PRIMARY KEY (order_number, user_id),
-  FOREIGN KEY (user_id) REFERENCES users(id)
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
