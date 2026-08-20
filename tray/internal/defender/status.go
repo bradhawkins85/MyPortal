ef4d9b82b4fd85a6079cedae1fccc4548d58fc34
bradhawkins85 <bradhawkins@gmail.com>
2026-08-20 13:15:49 +1000

    Fix tray Defender status reporting

--- tray/internal/defender/status.go
+++ tray/internal/defender/status.go
@@ -2,7 +2,10 @@
 package defender
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
+	"fmt"
 
 	"github.com/bradhawkins85/myportal-tray/internal/api"
 )
@@ -13,4 +16,13 @@ var ErrUnsupported = errors.New("Microsoft Defender status is only supported on 
 // Collect returns the current Microsoft Defender status.
 func Collect() (api.DefenderStatus, error) {
 	return collect()
+}
+
+func decodeStatus(output []byte) (api.DefenderStatus, error) {
+	var status api.DefenderStatus
+	output = bytes.TrimSpace(bytes.TrimPrefix(output, []byte{0xef, 0xbb, 0xbf}))
+	if err := json.Unmarshal(output, &status); err != nil {
+		return api.DefenderStatus{}, fmt.Errorf("decode Microsoft Defender status: %w", err)
+	}
+	return status, nil
 }
