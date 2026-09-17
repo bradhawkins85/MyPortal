ca13e437b20f6944ec7a87b83c28d57d011ff965
bradhawkins85 <bradhawkins@gmail.com>
2026-09-17 14:24:28 +1000

    Fix M365 spam purge page rendering

--- app/features/m365_admin/__init__.py
+++ app/features/m365_admin/__init__.py
@@ -15,7 +15,7 @@ from .routes import router as ui_router
 
 PACK = FeaturePack(
     slug="m365_admin",
-    version="1.2.3",
+    version="1.2.4",
     routers=(api_router, oof_router, ui_router),
 )
 
