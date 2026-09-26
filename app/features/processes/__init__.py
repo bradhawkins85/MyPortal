0bf6081eed400dccadcf9cea2cafbad949d36e7b
bradhawkins85 <bradhawkins@gmail.com>
2026-09-26 20:03:48 +1000

    Add process template and run web UI

--- app/features/processes/__init__.py
+++ app/features/processes/__init__.py
@@ -2,8 +2,8 @@
 
 from app.core.features import FeaturePack
 
-from .routes import router
+from .routes import router, web_router
 
-PACK = FeaturePack(slug="processes", version="1.0.0", routers=(router,))
+PACK = FeaturePack(slug="processes", version="1.1.0", routers=(router, web_router))
 
 __all__ = ["PACK"]
